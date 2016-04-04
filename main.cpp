73f1a07170f318bcb29e8f39a018169031fca354
HenryZhou7 <zhouyh.7@gmail.com>
2016-04-04 14:23:53 -0400

    Completely buffled
    Can I create a project and user source control on the whole thing? Such as its build file and stuff?

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  GitHub Repository test
+//
+//  Created by Henry Zhou on 2016-04-04.
+//  Copyright © 2016 Henry Zhou. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    cout << "Hello World!" << endl;
+    
+    return 0;
+}
